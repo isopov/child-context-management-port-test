611f0b5cc80beafe5fead123673375b2d9dd4ed8
Ivan Sopov <i.sopov@tinkoff.ru>
2018-06-14 19:27:53 +0300

    move management autoconfig from parent to child that fixes the problem

--- src/main/java/com/example/managementporttest/child/ChildModule.java
+++ src/main/java/com/example/managementporttest/child/ChildModule.java
@@ -1,5 +1,6 @@
 package com.example.managementporttest.child;
 
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementContextAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
@@ -7,7 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-@ImportAutoConfiguration(EmbeddedWebServerFactoryCustomizerAutoConfiguration.class)
+@ImportAutoConfiguration({
+        EmbeddedWebServerFactoryCustomizerAutoConfiguration.class,
+        ManagementContextAutoConfiguration.class
+})
 public class ChildModule {
     public static final String CHILD_PROFILE = "child";
 }
